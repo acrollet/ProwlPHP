f233040d33bb2ad7770f318989d198043693117f
Fenric <nathan@fenric.co.uk>
2009-07-09 11:57:37 -0700

    Fraking did it again, left personal API key in code. Sorry!

--- example.php
+++ example.php
@@ -1,12 +1,11 @@
 <?php
 
 include('ProwlPHP.php');
-$prowl = new Prowl('e9fae91305527666a8a73de3f2542a299e64d9b3');
+$prowl = new Prowl('APIKEY');
 $prowl->push(array(
                 'application'=>'Application',
                 'event'=>'Event',
                 'description'=>"Description",
                 'priority'=>0
             ));
-var_dump($prowl->getError());
 ?>
